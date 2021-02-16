1f8000b5e200857d6e843c72ec8aba862ade50e1
Lambert Chu <lambert.chu@gmail.com>
2021-02-16 11:10:44 -0500

    Update tasks to be used as objects

--- surge/errors.py
+++ surge/errors.py
@@ -1,6 +1,20 @@
 class SurgeRequestError(Exception):
-    """Raise for exceptions that occur during authentication"""
+    """Catch-all exception for errors that occur during request"""
     def __init__(self, message="Something went wrong with the API request."):
+        self.message = message
+        super().__init__(self.message)
+
+
+class SurgeMissingAPIKeyError(Exception):
+    """Raise when user has not set their API key"""
+    def __init__(self, message="Surge API key has not been set."):
+        self.message = message
+        super().__init__(self.message)
+
+
+class SurgeMissingIDError(Exception):
+    """Raise for exceptions that occur during authentication"""
+    def __init__(self, message="Must be initialized with an id."):
         self.message = message
         super().__init__(self.message)
 
@@ -8,8 +22,8 @@ class SurgeRequestError(Exception):
 class SurgeProjectQuestionError(Exception):
     """Raise for exceptions that occur when adding Questions to a new Project"""
     def __init__(
-        self,
-        message="All questions added to new Project must be of type Question."
+            self,
+            message="All questions added to a Project must be of type Question."
     ):
         self.message = message
         super().__init__(self.message)
--- surge/tasks.py
+++ surge/tasks.py
@@ -2,16 +2,25 @@ from surge.api_resource import PROJECTS_ENDPOINT, TASKS_ENDPOINT, APIResource
 
 
 class Task(APIResource):
-    def __init__(self):
+    def __init__(self, **kwargs):
         super().__init__()
+        self.__dict__.update(kwargs)
+        assert self.id is not None
+        assert self.project_id is not None
+
+    def __str__(self):
+        return f"SurgeTask_{self.id}"
 
     @classmethod
     def list(cls, project_id, page_num=1):
         endpoint = f"{PROJECTS_ENDPOINT}/{project_id}/{TASKS_ENDPOINT}"
         params = {"page": page_num}
-        return cls.get(endpoint, params)
+        response_json = cls.get(endpoint, params)
+        tasks = [cls(**task_json) for task_json in response_json]
+        return tasks
 
     @classmethod
     def retrieve(cls, task_id):
         endpoint = f"{TASKS_ENDPOINT}/{task_id}"
-        return cls.get(endpoint)
+        response_json = cls.get(endpoint)
+        return cls(**response_json)
