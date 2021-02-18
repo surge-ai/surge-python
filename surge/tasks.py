9e022c1101d2e3917b57433f757465d30569d495
Lambert Chu <lambert.chu@gmail.com>
2021-02-18 16:29:52 -0500

    Add error handling for tasks_data param

--- surge/tasks.py
+++ surge/tasks.py
@@ -1,4 +1,4 @@
-from surge.errors import SurgeMissingIDError
+from surge.errors import SurgeMissingIDError, SurgeTaskDataError
 from surge.api_resource import PROJECTS_ENDPOINT, TASKS_ENDPOINT, APIResource
 from surge.responses import TaskResponse
 
@@ -32,6 +32,23 @@ class Task(APIResource):
 
     @classmethod
     def create_many(cls, project_id: str, tasks_data: list):
+        '''
+        Creates new Task objects for a given Project.
+
+            Parameters:
+                project_id (str): ID of the project to which the tasks are added
+                tasks_data (list): list of dicts that map each task field to its value
+                    e.g. [{"website": "surgehq.ai"}, {"website":"twitch.tv"}]
+
+            Returns:
+                tasks (list): list of Task objects
+        '''
+        if type(tasks_data) is not list or len(tasks_data) == 0:
+            raise SurgeTaskDataError
+
+        if not all(isinstance(t, dict) for t in tasks_data):
+            raise SurgeTaskDataError
+
         endpoint = f"{PROJECTS_ENDPOINT}/{project_id}/{TASKS_ENDPOINT}/create_tasks"
         data = {"tasks": tasks_data}
         response_json = cls.post(endpoint, data)
