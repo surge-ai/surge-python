ed1fbbea321f1cb72c691ab8ec45755b5ecf8105
Lambert Chu <lambert.chu@gmail.com>
2021-02-22 16:37:22 -0500

    Fix fields_template bug and add docstrings

--- surge/projects.py
+++ surge/projects.py
@@ -38,6 +38,25 @@ class Project(APIResource):
                callback_url: str = None,
                fields_template: str = None,
                num_workers_per_task: int = 1):
+        '''
+        Creates a new Project.
+
+        Arguments:
+            name (str): Name of the project.
+            payment_per_response (float, optional):
+                How much a worker is paid (in US dollars) for an individual response.
+            private_workforce (bool, optional):
+                Indicates if the project's tasks will be done by a private workforce.
+            instructions (str, optional): Instructions shown to workers describing how they should complete the task.
+            questions (list, optional): An array of question objects describing the questions to be answered.
+            callback_url (str, optional): url that receives a POST request with the project's data.
+            fields_template (str, optional): A template describing how fields are shown to workers working on the task.
+                For example, if fields_template is "{{company_name}}", then workers will be shown a link to the company.
+            num_workers_per_task (int, optional): How many workers work on each task (i.e., how many responses per task).
+
+        Returns:
+            project: new Project object
+        '''
 
         # Convert list of question objects into dicts in valid json format
         # If this isn't a list of Question objects, throw an exception
@@ -52,7 +71,7 @@ class Project(APIResource):
             "instructions": instructions,
             "questions": questions_json,
             "callback_url": callback_url,
-            "field_template": fields_template,
+            "fields_template": fields_template,
             "num_workers_per_task": num_workers_per_task
         }
         if payment_per_response is not None:
@@ -62,6 +81,17 @@ class Project(APIResource):
 
     @classmethod
     def list(cls, page: int = 1):
+        '''
+        Lists all projects you have created.
+        Projects are returned in descending order of created_at.
+        Each page contains a maximum of 25 projects.
+
+        Arguments:
+            page (int, optional): Page number to retrieve. Pages start at 1 (default value).
+
+        Returns:
+            projects (list): list of Project objects.
+        '''
         params = {"page": page}
         response_json = cls.get(PROJECTS_ENDPOINT, params)
         projects = [cls(**project_json) for project_json in response_json]
@@ -69,38 +99,87 @@ class Project(APIResource):
 
     @classmethod
     def retrieve(cls, project_id: str):
+        '''
+        Retrieves a specific project you have created.
+
+        Arguments:
+            project_id (str): ID of project.
+
+        Returns:
+            project: Project object
+        '''
         endpoint = f"{PROJECTS_ENDPOINT}/{project_id}"
         response_json = cls.get(endpoint)
         return cls(**response_json)
 
     def pause(self):
+        '''
+        Pauses a project.
+        Tasks added to the project will not be worked on until you resume the project.
+
+        Returns:
+            project: new Project object with updated status
+        '''
         endpoint = f"{PROJECTS_ENDPOINT}/{self.id}/pause"
         return self.put(endpoint)
 
     def resume(self):
+        '''
+        Resumes a paused project.
+
+        Returns:
+            project: new Project object with updated status
+        '''
         endpoint = f"{PROJECTS_ENDPOINT}/{self.id}/resume"
         return self.put(endpoint)
 
     def cancel(self):
+        '''
+        Cancels a project.
+
+        Returns:
+            project: new Project object with updated status
+        '''
         endpoint = f"{PROJECTS_ENDPOINT}/{self.id}/cancel"
         return self.put(endpoint)
 
     def list_tasks(self, page: int = 1):
+        '''
+        Lists all tasks belonging to this project.
+        Tasks are returned in ascending order of created_at.
+        Each page contains a maximum of 25 tasks.
+
+        Arguments:
+            page (int, optional): Page number to retrieve. Pages start at 1 (default value).
+
+        Returns:
+            tasks (list): list of Task objects.
+        '''
         return Task.list(self.id, page=page)
 
     def create_tasks(self, tasks_data: list):
         '''
-        Creates new Task objects for the current Project.
+        Creates new Task objects for this project.
 
-            Parameters:
-                tasks_data (list): list of dicts that map each task field to its value
-                    e.g. [{"website": "surgehq.ai"}, {"website":"twitch.tv"}]
+        Arguments:
+            tasks_data (list): list of dicts that map each task field to its value
+                e.g. [{"website": "surgehq.ai"}, {"website":"twitch.tv"}]
 
-            Returns:
-                tasks (list): list of Task objects
+        Returns:
+            tasks (list): list of Task objects
         '''
         return Task.create_many(self.id, tasks_data)
 
     def create_tasks_from_csv(self, file_path: str):
+        '''
+        Creates new Task objects for this project from a local CSV file.
+        The header of the CSV file must specify the fields that are used in your Tasks.
+
+        Arguments:
+            file_path (str): path to CSV file.
+
+        Returns:
+            tasks (list): list of Task objects
+        '''
         tasks_data = utils.load_tasks_data_from_csv(file_path)
         return self.create_tasks(tasks_data)
--- surge/tasks.py
+++ surge/tasks.py
@@ -38,13 +38,14 @@ class Task(APIResource):
     def set_gold_standard(self,
                           gold_standard_answers=None,
                           is_gold_standard=True):
-        '''Set gold standard answers for this task.
+        '''
+        Set gold standard answers for this task.
 
-        Arguements:
-        gold_standard_answers (List[string]): A list of the ground truth answers for this task, one for each question in the project.
-            If you don't want to set an answer for one of the questions, you can leave it blank by passing an empty string.
-        is_gold_standard (boolean): This indicates whether this task is a gold standard. You can toggle gold standards on or off by
-            setting this as true or false.
+        Arguments:
+            gold_standard_answers (List[string]): A list of the ground truth answers for this task, one for each question in the project.
+                If you don't want to set an answer for one of the questions, you can leave it blank by passing an empty string.
+            is_gold_standard (boolean): This indicates whether this task is a gold standard. You can toggle gold standards on or off by
+                setting this as true or false.
         '''
         if self.id is None or self.project_id is None:
             raise SurgeMissingIDError
@@ -57,6 +58,16 @@ class Task(APIResource):
 
     @classmethod
     def create(cls, project_id: str, **params):
+        '''
+        Creates a new Task object for a given project.
+
+        Arguments:
+            project_id (str): ID of the project to which the tasks are added.
+            **params: Additional keyword arguments.
+
+        Returns:
+            task: new Task object
+        '''
         endpoint = f"{PROJECTS_ENDPOINT}/{project_id}/{TASKS_ENDPOINT}"
         data = {"fields": params}
         response_json = cls.post(endpoint, data)
@@ -65,15 +76,15 @@ class Task(APIResource):
     @classmethod
     def create_many(cls, project_id: str, tasks_data: list):
         '''
-        Creates new Task objects for a given Project.
+        Creates new Task objects for a given project.
 
-            Parameters:
-                project_id (str): ID of the project to which the tasks are added
-                tasks_data (list): list of dicts that map each task field to its value
-                    e.g. [{"website": "surgehq.ai"}, {"website":"twitch.tv"}]
+        Arguments:
+            project_id (str): ID of the project to which the tasks are added.
+            tasks_data (list): list of dicts that map each task field to its value.
+                e.g. [{"website": "surgehq.ai"}, {"website":"twitch.tv"}]
 
-            Returns:
-                tasks (list): list of Task objects
+        Returns:
+            tasks (list): list of Task objects
         '''
         if type(tasks_data) is not list or len(tasks_data) == 0:
             raise SurgeTaskDataError
@@ -89,6 +100,18 @@ class Task(APIResource):
 
     @classmethod
     def list(cls, project_id: str, page: int = 1):
+        '''
+        Lists all tasks belonging to a given project.
+        Tasks are returned in ascending order of created_at.
+        Each page contains a maximum of 25 tasks.
+
+        Arguments:
+            project_id (str): ID of project.
+            page (int, optional): Page number to retrieve. Pages start at 1 (default value).
+
+        Returns:
+            tasks (list): list of Task objects.
+        '''
         endpoint = f"{PROJECTS_ENDPOINT}/{project_id}/{TASKS_ENDPOINT}"
         params = {"page": page}
         response_json = cls.get(endpoint, params)
@@ -97,6 +120,15 @@ class Task(APIResource):
 
     @classmethod
     def retrieve(cls, task_id: str):
+        '''
+        Retrieves a specific task you have created.
+
+        Arguments:
+            task_id (str): ID of task.
+
+        Returns:
+            task: Task object
+        '''
         endpoint = f"{TASKS_ENDPOINT}/{task_id}"
         response_json = cls.get(endpoint)
         return cls(**response_json)
