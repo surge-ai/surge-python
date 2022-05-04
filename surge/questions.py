d234ef8e1d6e9866a489b5740acf671c56e50617
Andrew Mauboussin <andrew@surgehq.ai>
2022-05-04 15:48:14 -0700

    Support additional question types and question params

--- surge/questions.py
+++ surge/questions.py
@@ -15,36 +15,55 @@ class Question(object):
 
 
 class FreeResponseQuestion(Question):
-    def __init__(self, text, required=True):
+    def __init__(self, text, required=True, preexisting_annotations=None):
         super().__init__(text, type_="free_response", required=required)
+        self.preexisting_annotations = preexisting_annotations
 
 
 class MultipleChoiceQuestion(Question):
-    def __init__(self, text, options=[], descriptions=[], required=True):
+    def __init__(self, text, options=[], descriptions=[], required=True, preexisting_annotations=None, require_tiebreaker=False):
         super().__init__(text, type_="multiple_choice", required=required)
         self.options = options
         self.descriptions = descriptions
-
+        self.preexisting_annotations = preexisting_annotations
+        self.require_tiebreaker = require_tiebreaker
 
 class CheckboxQuestion(Question):
-    def __init__(self, text, options=[], descriptions=[], required=True):
+    def __init__(self, text, options=[], descriptions=[], required=True, preexisting_annotations=None, require_tiebreaker=False):
         super().__init__(text, type_="checkbox", required=required)
         self.options = options
         self.descriptions = descriptions
+        self.preexisting_annotations = preexisting_annotations
+        self.require_tiebreaker = require_tiebreaker
 
 
 class TextTaggingQuestion(Question):
-    def __init__(self, text, options=[], required=True):
-        super().__init__(text, type_="text_tagging", required=required)
+    def __init__(self, text, options=[], preexisting_annotations=None, token_granularity=True, allow_relationship_tags=False, allow_overlapping_tags=False):
+        super().__init__(text, type_="text_tagging", required=False)
         self.options = options
-
+        self.preexisting_annotations = preexisting_annotations
+        self.token_granularity = token_granularity
+        self.allow_relationship_tags = allow_relationship_tags
+        self.allow_overlapping_tags = allow_overlapping_tags
 
 class TreeSelectionQuestion(Question):
-    def __init__(self, text, options=[], descriptions=[], required=True):
+    def __init__(self, text, options=[], descriptions=[], required=True, preexisting_annotations=None):
         super().__init__(text, type_="tree_selection", required=required)
         self.options = options
         self.descriptions = descriptions
+        self.preexisting_annotations = preexisting_annotations
 
+class RankingQuestion(Question):
+    def __init__(self, text, options=[]):
+        super().__init__(text, type_="ranking", required=False)
+        self.options = options
+
+class ChatBot(Question):
+    def __init__(self, text, options=[], endpoint_url=None, endpoint_headers=None):
+        super().__init__(text, type_="chat", required=False)
+        self.options = options
+        self.endpoint_url = endpoint_url
+        self.endpoint_headers = endpoint_headers
 
 class TextArea(Question):
     def __init__(self, text):
