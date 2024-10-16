7a360e19d0256333cdfb06b03d7fa5d28e549ae8
Tim Bauman <tim@surgehq.ai>
2024-10-16 15:34:10 -0700

    Add support for post file uploads

--- tests/test_api_resource.py
+++ tests/test_api_resource.py
@@ -1,3 +1,5 @@
+from io import StringIO
+from unittest import mock
 import pytest
 import requests
 
@@ -24,10 +26,47 @@ def test_raise_exception_if_invalid_api_key():
         APIResource._base_request("get", surge.api_resource.PROJECTS_ENDPOINT)
 
 
-def test_passed_in_api_key(mocker):
-    mock_request = mocker.patch.object(requests, 'get')
-    APIResource._base_request("get", surge.api_resource.PROJECTS_ENDPOINT, api_key="passed_api_key")
-    mock_request.assert_called_once_with("https://app.surgehq.ai/api/projects", auth=("passed_api_key", ""), params=None)
+def test_passed_in_api_key():
+    with mock.patch.object(requests, "get") as mock_request:
+        mock_request.return_value = mock.MagicMock()
+        APIResource._base_request(
+            "get", surge.api_resource.PROJECTS_ENDPOINT, api_key="passed_api_key"
+        )
+        mock_request.assert_called_once_with(
+            "https://app.surgehq.ai/api/projects",
+            auth=("passed_api_key", ""),
+            params=None,
+        )
+
+
+def test_passed_in_file():
+    with mock.patch.object(requests, "post") as mock_request:
+        files = {"file": StringIO()}
+        mock_request.return_value = mock.MagicMock()
+        APIResource._base_request(
+            "post",
+            surge.api_resource.PROJECTS_ENDPOINT,
+            files=files,
+            api_key="passed_api_key",
+        )
+        mock_request.assert_called_once_with(
+            "https://app.surgehq.ai/api/projects",
+            auth=("passed_api_key", ""),
+            files=files,
+            json=None,
+        )
+
+
+def test_get_passed_in_file():
+    with pytest.raises(SurgeRequestError) as e_info:
+        files = {"file": StringIO()}
+        APIResource._base_request(
+            "get",
+            surge.api_resource.PROJECTS_ENDPOINT,
+            files=files,
+            api_key="passed_api_key",
+        )
+
 
 def test_print_attrs():
     a1 = APIResource(id="ABC1234").print_attrs()
