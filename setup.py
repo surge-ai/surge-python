b1d92c242c5cd989b8b6ea790b9f72d4d6255045
Tim Bauman <tim@surgehq.ai>
2024-10-17 16:57:26 -0700

    New version

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.5.9'
+VERSION = "1.5.10"
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-with open('README.md', 'r') as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='surge-api',
-      version=VERSION,
-      description='Surge Python SDK',
-      author='Surge',
-      author_email='team@surgehq.ai',
-      url='https://github.com/surge-ai/surge-python',
-      license='MIT',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      python_requires='>=3.6',
-      packages=find_packages(exclude=['tests', 'tests.*']),
-      install_requires=requirements,
-      tests_require=['pytest >= 6.0.0'])
+setup(
+    name="surge-api",
+    version=VERSION,
+    description="Surge Python SDK",
+    author="Surge",
+    author_email="team@surgehq.ai",
+    url="https://github.com/surge-ai/surge-python",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=requirements,
+    tests_require=["pytest >= 6.0.0"],
+)
