2f673cf9428840a69bb421e74758a61703abc47b
Tim Bauman <tim@surgehq.ai>
2024-12-27 13:13:52 -0800

    Add additional methods to help with downloads

--- surge/reports.py
+++ surge/reports.py
@@ -3,8 +3,9 @@ from time import sleep
 import urllib
 import tempfile
 import shutil
+import io
+import json
 
-from surge.errors import SurgeMissingIDError, SurgeMissingAttributeError
 from surge.api_resource import REPORTS_ENDPOINT, APIResource
 
 
@@ -24,60 +25,96 @@ class Report(APIResource):
         return self.print_attrs(forbid_list=["id"])
 
     @classmethod
-    def save_report(cls,
-                    project_id: str,
-                    type: str,
-                    filepath=None,
-                    poll_time=30,
-                    api_key: str = None):
-        '''
+    def save_report(
+        cls,
+        project_id: str,
+        type: str,
+        filepath=None,
+        poll_time=5 * 60,
+        api_key: str = None,
+    ):
+        """
         Request creation of a report, poll until the report is generated, and save the data to a file all in one call.
-        Args:
-        project_id (string): UUID of project to get data for
-        type (string): Must be one of these types:
-          * `export_json`
-          * `export_json_aggregated`
-          * `export_csv`
-          * `export_csv_aggregated`
-          * `export_csv_flattened`
-        filepath (string or None): Location to save the results file. If not specified, will save to "project_{project_id}_results.{csv/json}
-        poll_time (int): Number of seconds to poll for the report
-      '''
+        Arguments:
+            project_id (string): UUID of project to get data for
+            type (string): Must be one of these types:
+              * `export_json`
+              * `export_json_aggregated`
+              * `export_csv`
+              * `export_csv_aggregated`
+              * `export_csv_flattened`
+            filepath (string or IO or None): Location to save the results file. If not specified, will save to "project_{project_id}_results.{csv/json}
+            poll_time (int): Number of seconds to poll for the report
+        """
         for _ in range(poll_time // 2):
-            response = cls.request(project_id=project_id,
-                                   type=type,
-                                   api_key=api_key)
+            response = cls.request(project_id=project_id, type=type, api_key=api_key)
             # Download zipped project results if ready
             if response.status == "READY":
                 file_ext = "csv" if "csv" in type else "json"
-                default_file_name = "project_{project_id}_results.{file_ext}.gzip".format(
-                    project_id=project_id, file_ext=file_ext)
+                default_file_name = (
+                    "project_{project_id}_results.{file_ext}.gzip".format(
+                        project_id=project_id, file_ext=file_ext
+                    )
+                )
                 with urllib.request.urlopen(response.url) as response:
                     with tempfile.NamedTemporaryFile() as tmp_file:
                         shutil.copyfileobj(response, tmp_file)
                         tmp_file.flush()
                         # Unzip and save results
                         data = gzip.open(tmp_file.name, "r").read()
-                        open(filepath or default_file_name.rstrip('.gzip'),
-                             "wb").write(data)
-                return None
+                        filepath = filepath or default_file_name.rstrip(".gzip")
+                        if isinstance(filepath, str):
+                            file = open(
+                                filepath or default_file_name.rstrip(".gzip"), "wb"
+                            )
+                        else:
+                            file = filepath
+                        file.write(data)
+                        if isinstance(filepath, str):
+                            file.close()
+                return
 
             # Wait two seconds before polling again
-            elif response.status == 'CREATING':
+            elif response.status == "CREATING":
                 sleep(2)
                 continue
             else:
                 raise ValueError(
-                    "Report failed to generate with status {}".format(
-                        response.status))
+                    "Report failed to generate with status {}".format(response.status)
+                )
 
-            raise Exception(
-                "Report failed to generate within {poll_time} seconds".format(
-                    poll_time=poll_time))
+        raise Exception(
+            "Report failed to generate within {poll_time} seconds".format(
+                poll_time=poll_time
+            )
+        )
+
+    @classmethod
+    def download_json(cls, project_id: str, poll_time=5 * 60, api_key: str = None):
+        """
+        Download and parse the results JSON for a project
+
+        Arguments:
+            project_id (string): UUID of project to get data for
+            poll_time (int): Number of seconds to poll for the report
+
+        Returns:
+            results (list): List of dictionaries of results for each response
+        """
+        bytesio = io.BytesIO()
+        cls.save_report(
+            project_id=project_id,
+            type="export_json",
+            filepath=bytesio,
+            poll_time=poll_time,
+            api_key=api_key,
+        )
+        bytesio.seek(0)
+        return json.load(bytesio)
 
     @classmethod
     def request(cls, project_id: str, type: str, api_key: str = None):
-        '''
+        """
         Request creation of a report for the given type. Note that reports are generated
         asychronously so the response may include a `job_id` which needs to be used with
         the `status` method to get the job status. In the event that the report has is
@@ -97,7 +134,7 @@ class Report(APIResource):
 
           {
             status: "CREATING",
-            job_id: ...,            
+            job_id: ...,
           }
 
         2) The report is already generated and up to date:
@@ -106,7 +143,7 @@ class Report(APIResource):
             status: "READY",
             url: ...,
             expires_in_seconds: ...,
-          }          
+          }
 
         Arguments:
             project_id (str): ID of project.
@@ -114,7 +151,7 @@ class Report(APIResource):
 
         Returns:
             status: Report status object which includes report id
-        '''
+        """
         endpoint = f"{REPORTS_ENDPOINT}/{project_id}/report"
         params = {"report_type": type}
         response_json = cls.post(endpoint, params, api_key=api_key)
@@ -122,7 +159,7 @@ class Report(APIResource):
 
     @classmethod
     def status(cls, project_id: str, job_id: str, api_key: str = None):
-        '''
+        """
         Checks the status of a given report job. The response will be of one of these shapes:
 
         1) Report is still being generated (HTTP 202):
@@ -132,7 +169,7 @@ class Report(APIResource):
           }
 
         2) Report is completed:
-        
+
           {
             status: "COMPLETED",
             url: ...,
@@ -141,7 +178,7 @@ class Report(APIResource):
 
         3) Retrying generation of report (can consider this to be same as in progress):
 
-          {            
+          {
             status: "RETRYING",
             job_id: ...,
           }
@@ -159,7 +196,7 @@ class Report(APIResource):
 
         Returns:
             status: Report status object
-        '''
+        """
         endpoint = f"{REPORTS_ENDPOINT}/{project_id}/report_status"
         params = {"job_id": job_id}
         response_json = cls.get(endpoint, params, api_key=api_key)
